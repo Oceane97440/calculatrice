8ccb5a2db66d5d76ba6c55918b7486a79933eed2
oceane97440 <sautronoceane98@gmail.com>
2020-10-26 15:43:16 +0400

    calcul function

---
+++ public/admin/js/function.calc.js
@@ -0,0 +1,34 @@
+  //fonction qui évalue le chiffre et renvoie la sortie
+  function calculer() {
+      let a = document.getElementById("output").value
+      let b = eval(a)
+      document.getElementById("output").value = b
+
+      localStorage.setItem(a, b);
+
+      // Accéder à des données enregistrées
+      alert("resultat = " + localStorage.getItem(a));
+      document.getElementById("resultat").value=b
+
+      
+      var nbr_resultat= localStorage.length
+      console.log(nbr_resultat)
+
+    
+
+      if (nbr_resultat>10) {
+        
+        alert("historique limité à 10 resultat" + localStorage.clear());
+
+      }
+
+   
+  }
+  //fonction qui affiche la valeur
+  function afficher(val) {
+      document.getElementById("output").value += val
+  }
+  //fonction qui efface l'écran 
+  function effacer() {
+      document.getElementById("output").value = ""
+  }
